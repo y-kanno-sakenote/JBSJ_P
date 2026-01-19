74ff9bd24e824366a199bf64ab9870908ee51652
y-kanno-sakenote <y.kanno@sakenote.net>
2026-01-19 14:52:11 +0900

    1

--- app.py
+++ app.py
@@ -377,7 +377,7 @@ def make_row_id(row):
     return f"T:{ttl}|Y:{yr}"
 
 # -------------------- データ読み込み --------------------
-st.title("醸造協会誌　論文検索 β_3.0")
+st.title("醸造協会誌　論文検索 β_4.0")
 
 DEMO_CSV_PATH = Path("data/keywords_summary5.csv")   # メインCSV
 SUMMARY_CSV_PATH = Path("data/summaries.csv")         # ← 追加: summary
